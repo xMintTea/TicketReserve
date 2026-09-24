14c4dd9c0e52b8973d6ee0cfb901147791b26cb7
xMintTea <minttea77105110116@gmail.com>
2026-09-24 22:36:28 +0800

    feat: update project structure

--- app/fronend/src/routeTree.gen.ts
+++ app/fronend/src/routeTree.gen.ts
@@ -8,9 +8,9 @@
 // You should NOT make any changes in this file as it will be overwritten.
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
-import { Route as rootRouteImport } from './routes/__root'
+import { Route as rootRouteImport } from './routes/__root.tsx'
 import { Route as IndexRouteImport } from './routes/index'
-import { Route as MoviesRouteImport } from './routes/movies'
+import { Route as MoviesRouteImport } from './routes/movies.tsx'
 
 const IndexRoute = IndexRouteImport.update({
   id: '/',
