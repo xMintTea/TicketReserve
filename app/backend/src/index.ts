57cdcbac026ce5dffd01cfaa9345956585e9647d
xMintTea <minttea77105110116@gmail.com>
2026-09-26 00:42:52 +0800

    feat: add drizzle instance

--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -1,6 +1,11 @@
 import { Hono } from 'hono'
+import { drizzle } from 'drizzle-orm/node-postgres'
+import 'dotenv/config';
+
+
 
 const app = new Hono()
+const db = drizzle(process.env.DATABASE_URL!);
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
